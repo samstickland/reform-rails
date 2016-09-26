9dadbb1fd1f7371e94545ce3d826cba5d862147f
Fran Worley <frances@safetytoolbox.co.uk>
2016-09-26 14:59:09 +0100

    support validates_each (AMV)

--- lib/reform/form/active_model/validations.rb
+++ lib/reform/form/active_model/validations.rb
@@ -50,7 +50,7 @@ module Reform::Form::ActiveModel
       end
 
       extend Uber::Delegates
-      delegates :@validations, :validates, :validate, :validates_with, :validate_with
+      delegates :@validations, :validates, :validate, :validates_with, :validate_with, :validate_each, :validates_each
 
       def call(form, errors) # FIXME.
         validator = @validations.new(form)
